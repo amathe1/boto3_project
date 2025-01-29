f774a998d774f1d3d9a311d2c38cf20145df3059
amathe1 <amathe1@apple.com>
2025-01-29 15:10:36 +0530

    boto3

--- boto3_module/boto3_examples.py
+++ boto3_module/boto3_examples.py
@@ -1,3 +1,19 @@
 import boto3
 
-print("Boto3")
+# Create a boto3 session  
+session = boto3.Session(  
+    aws_access_key_id='xxxx-xxxx-xxxx-xxxx',  
+    aws_secret_access_key='xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx-xxxx',  
+    region_name='ap-south-1'  
+)  
+
+
+# Create an S3 client  
+s3 = session.client('s3') 
+
+# List all the buckets in your account  
+response = s3.list_buckets()  
+
+# Print the bucket names  
+for bucket in response['Buckets']:  
+    print(bucket['Name'])  
